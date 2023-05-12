58d8232d45915f66a2acc591760321e0298bcb4a
xtonik <antonin.janec@gmail.com>
2023-05-12 14:05:41 +0200

    Added some corner test cases and fixes for them. Tiny performance improvements.

--- src/test/java/cz/janec/floatparser/FloatParserTest.java
+++ src/test/java/cz/janec/floatparser/FloatParserTest.java
@@ -7,17 +7,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// TODO parametrize parsers
 public class FloatParserTest {
-    @ParameterizedTest
+
+    @ParameterizedTest(name = "{index}.  {0}")
+    //@MethodSource("provideStringsForIsBlank")
+    //@EnabledIf("comparsion")
     @ValueSource(strings = {
             "Infinity",
             "0x1p1", "0x1.2p3", "0x1.2P3", "0xAA.BCDp-3", "0xAA.BC0DP+3", // C99 floating-point hexadecimal
-            "1", "1.0", "1e0", "1e-0", // ones
+            "1", "1.", "1.0", "1e0", "1e-0", // ones
             "1f", "1F", "1D", "1d", // ones with Java data type literal suffixes
             "1e2f", "1e2F", "1e2D", "1e2d",
             ".1e2f", ".1e2F", ".1e2D", ".1e2d",
             "1.1e2f", "1.1e2F", "1.1e2D", "1.1e2d",
-            "0.0", "0.0e38", "0.0e-38", // zeroes
+            "1.e2f", "1.e2F", "1.e2D", "1.e2d",
+            "0", "0.", "0.0", "0.0e38", "0.0e-38", "0e999", "1.111111e-500", // zeroes
+            "10", "15", "111", "123", "1234", "9999999", // ordinary integers
+            // integers around 24 bits - float mantissa bit length:
+            "16777217", "16777216", "16777215", "999999999", // 2^24+1, 2^24, 2^24-1, >> 2^24
+            "1.23", "12.3", "123.0", "0.3", "0.123", "1645.34", // some ordinary decimal values
             ".0", ".1", ".01", ".0e2", ".1e2", ".01e2", // leading dot
             "1e+1", "0e+1", ".1e+2", "1.1e+2", "0.0e+2", "1.0e+2", "0.1e+2",
             "1e50", "1e100", "1e200", "123.456e50", "123.456e100", "123.456e200",
@@ -29,9 +38,12 @@ public class FloatParserTest {
             "000000000000012345678999E2", "000000000000012345678999E-2", "000000000000012345678999E+2",
             "0.0000000000000001e15", "0.0000000000000001e-15",
             "1000000000000000e-15", "1000000000000000e+15",
-            "1000000000000001e-10", "1000000000000001e+10",
+
+            "1000000000000001e-10", "1000000000000001e+10", // digit under precision
             "1.00000000000001E0", "1.00000000000001",
             "1.10000000000001E0", "1.10000000000001",
+            "1.4e-45", "14e-46", "140e-47", // Float.MIN_VALUE
+            "2e-45", "20e-46", // above Float.MIN_VALUE
             "7.038531E-26", // rounding error suppressed by exact match string
             // values very similar to those, which are affected by rounding error
             "70385310001E-36", "7038531000001E-38", "7038531000000001E-41",
@@ -41,7 +53,6 @@ public class FloatParserTest {
     public void valid(String s) {
         assertAll(
                 () -> assertEquals(Float.parseFloat(s), FloatParser.parseFloat(s)),
-                // test string preceded by sign character also
                 () -> assertEquals(Float.parseFloat("-" + s), FloatParser.parseFloat("-" + s)),
                 () -> assertEquals(Float.parseFloat("+" + s), FloatParser.parseFloat("+" + s))
         );
@@ -57,34 +68,38 @@ public class FloatParserTest {
     public void validRoundError(String s) {
         assertAll(
                 () -> assertEquals(Float.floatToIntBits(Float.parseFloat(s)) + 1, Float.floatToIntBits(FloatParser.parseFloat(s))),
-                // test string preceded by sign character also
                 () -> assertEquals(Float.floatToIntBits(Float.parseFloat("-" + s)) + 1, Float.floatToIntBits(FloatParser.parseFloat("-" + s))),
                 () -> assertEquals(Float.floatToIntBits(Float.parseFloat("+" + s)) + 1, Float.floatToIntBits(FloatParser.parseFloat("+" + s)))
         );
     }
 
-    @Test
-    public void notANumber() {
-        final String notANumber = "NaN";
-        assertEquals(Float.parseFloat(notANumber), FloatParser.parseFloat(notANumber));
+    @ParameterizedTest
+    @ValueSource(strings = {"NaN", "+NaN", "-NaN"})
+    public void notANumber(String notANumber) {
+        assertAll(
+                () -> assertEquals(Float.NaN, Float.parseFloat(notANumber)),
+                () -> assertEquals(Float.NaN, FloatParser.parseFloat(notANumber))
+        );
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @ParameterizedTest
     @ValueSource(strings = {
+            "NaN2", "Infinity2", // falsify seems to be special value
             "", " ", "  ", // empty strings
             "e", ".", ".0e", "0e", ".e", "0.0e", // some number parts are missing
             "0.0.0", "0.0.1", "0.1e1.", "0.1e.1", // double dot
             "1 1", // whitespace in the middle
             "0x0", "0xFF", // hexadecimal
             "1dd", "1ff", "1ll", "1DD", "1FF", "1LL", "1df", "1fd", // suffixes for C99 floating-point data types
-            "z" // invalid character
+            "z", // invalid character
+            "1e6fz", "1e6dz", // java types suffix is not last character
     })
     public void invalid(String s) {
         assertAll(
                 () -> assertThrows(NumberFormatException.class, () -> Float.parseFloat(s)),
                 () -> assertThrows(NumberFormatException.class, () -> FloatParser.parseFloat(s)),
-                // test also values preceded by sign characters
+
                 () -> assertThrows(NumberFormatException.class, () -> Float.parseFloat("-" + s)),
                 () -> assertThrows(NumberFormatException.class, () -> FloatParser.parseFloat("-" + s)),
 
@@ -105,6 +120,7 @@ public class FloatParserTest {
     // Conversion round-trip from float to String and vice versa, i.e. float_value = Float.parseFloat(Float.toString(float_value)).
     @Disabled // exhaustive test - takes too much time ~ 1 hour, it should be verified at least once for each platform
     @Test
+    // TODO test opposite direction? String -> float -> String
     public void allStringifiedFloats() {
         long start = System.currentTimeMillis();
         int from = Integer.MIN_VALUE;
@@ -137,6 +153,9 @@ public class FloatParserTest {
     @ParameterizedTest
     @ValueSource(strings = {"1 ", " 1 ", " 1",})
     public void whitespacesAroundAreIgnored(String s) {
-        assertEquals(Float.parseFloat(s), FloatParser.parseFloat(s));
+        assertAll(
+                () -> assertEquals(1, Float.parseFloat(s)),
+                () -> assertEquals(1, FloatParser.parseFloat(s))
+        );
     }
 }
